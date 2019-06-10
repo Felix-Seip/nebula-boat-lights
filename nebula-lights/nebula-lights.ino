a1072aa32e043eb408954b06f7e1299359b6da70
Felix Seip <felixseip@Felixs-MacBook-Pro.local>
2019-06-10 20:29:13 +0200

    Example for using FASTLEDs library

--- nebula-lights/nebula-lights.ino
+++ nebula-lights/nebula-lights.ino
@@ -1,9 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <FastLED.h>
+#define NUM_LEDs = 10
 
+CRGB leds[NUM_LEDs];
+
+void setup() {
+  Serial.begin(9600);
+  FastLED.addLeds<WS2812B, DATA_PIN>(leds, NUM_LEDs);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  for(int i = 0; i < NUM_LEDs; i++){
+    leds[i] = CRGB(255, 255, 255);
+    delay(500);
+  }
 }
